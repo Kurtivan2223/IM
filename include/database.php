90043902b3dc3e2349e0268c2be8376012799c62
Kurtivan2223 <105442868+Kurtivan2223@users.noreply.github.com>
2023-11-09 15:23:08 +0800

    Create database.php

---
+++ include/database.php
@@ -0,0 +1,11 @@
+<?php
+
+class Database extends PDO
+{
+    public function __construct()
+    {
+        parent::__construct("mysql:host=".get_config('db_auth_host')."", get_config('db_auth_user'), get_config('db_auth_pass'));
+        $this->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
+}
