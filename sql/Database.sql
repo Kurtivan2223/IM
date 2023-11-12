b9d36507bb3472b329d7999d7c4ba38cb06b712d
Kurtivan2223 <105442868+Kurtivan2223@users.noreply.github.com>
2023-11-13 00:16:49 +0800

    Fixing and Adding something..

--- sql/Database.sql
+++ sql/Database.sql
@@ -1,49 +1,87 @@
-CREATE TABLE `Account`  (
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Localhost
+ Source Server Type    : MySQL
+ Source Server Version : 100428 (10.4.28-MariaDB)
+ Source Host           : localhost:3306
+ Source Schema         : system
+
+ Target Server Type    : MySQL
+ Target Server Version : 100428 (10.4.28-MariaDB)
+ File Encoding         : 65001
+
+ Date: 13/11/2023 00:15:06
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account`  (
   `ID` int NOT NULL AUTO_INCREMENT,
-  `Username` varchar(255) NULL,
-  `Email` varchar(255) NULL,
-  `Password` varchar(255) NULL,
-  `Fname` varchar(255) NULL,
-  `Lname` varchar(255) NULL,
-  `Gender` enum('M','F') NULL,
-  `RegisterDate` datetime NULL,
-  PRIMARY KEY (`ID`)
-);
+  `Username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `Email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `Password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `Fname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `Lname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `Gender` enum('M','F') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `RegisterDate` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `Booking`  (
-  `BookID` varchar(255) NOT NULL,
-  `AccID` int NULL,
-  `FlightID` varchar(255) NULL,
-  `BookDate` datetime NULL,
-  `PassengerSeatNumber` varchar(255) NULL,
-  `CabinClass` varchar(255) NULL,
-  `TicketNumber` varchar(255) NULL,
-  `TicketFare` varchar(255) NULL,
-  PRIMARY KEY (`BookID`)
-);
+-- ----------------------------
+-- Table structure for booking
+-- ----------------------------
+DROP TABLE IF EXISTS `booking`;
+CREATE TABLE `booking`  (
+  `BookID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `AccID` int NOT NULL,
+  `FlightID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `BookDate` datetime NOT NULL,
+  `PassengerSeatNumber` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `CabinClass` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `TicketNumber` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `TicketFare` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`BookID`) USING BTREE,
+  INDEX `AccountID_FKey`(`AccID` ASC) USING BTREE,
+  INDEX `FlightID_FKey`(`FlightID` ASC) USING BTREE,
+  CONSTRAINT `AccountID_FKey` FOREIGN KEY (`AccID`) REFERENCES `account` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FlightID_FKey` FOREIGN KEY (`FlightID`) REFERENCES `flight` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `Flight`  (
-  `ID` varchar(255) NOT NULL,
-  `FlightName` varchar(255) NULL,
-  `FlightSchedule` datetime NULL,
-  `PassengerCount` int NULL,
-  `Origin` varchar(255) NULL,
-  `Distination` varchar(255) NULL,
-  `BoardingTime` datetime NULL,
-  `DepartureTime` datetime NULL,
-  `TicketFare` enum('2500','5000','10000') NULL,
-  PRIMARY KEY (`ID`)
-);
+-- ----------------------------
+-- Table structure for flight
+-- ----------------------------
+DROP TABLE IF EXISTS `flight`;
+CREATE TABLE `flight`  (
+  `ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `FlightName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `FlightSchedule` datetime NULL DEFAULT NULL,
+  `PassengerCount` int NULL DEFAULT NULL,
+  `Origin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `Distination` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `BoardingTime` datetime NULL DEFAULT NULL,
+  `DepartureTime` datetime NULL DEFAULT NULL,
+  `TicketFare` enum('2500','5000','10000') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `Active` enum('Y','N') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `SupportTicket`  (
-  `TicketNo` varchar(255) NOT NULL,
-  `UserId` int NULL,
-  `Username` varchar(255) NULL,
-  `Message` varchar(255) NULL,
-  `ResolvedIssue` enum('Yes','No') NULL,
-  PRIMARY KEY (`TicketNo`)
-);
+-- ----------------------------
+-- Table structure for supportticket
+-- ----------------------------
+DROP TABLE IF EXISTS `supportticket`;
+CREATE TABLE `supportticket`  (
+  `TicketNo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `UserId` int NULL DEFAULT NULL,
+  `Username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `Message` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `ResolvedIssue` enum('Yes','No') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`TicketNo`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
-ALTER TABLE `Booking` ADD CONSTRAINT `AccountID_FKey` FOREIGN KEY (`AccID`) REFERENCES `Account` (`ID`);
-ALTER TABLE `Booking` ADD CONSTRAINT `FlightID_FKey` FOREIGN KEY (`FlightID`) REFERENCES `Flight` (`ID`);
-
+SET FOREIGN_KEY_CHECKS = 1;
