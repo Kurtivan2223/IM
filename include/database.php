cb9ba1eed6cde33bead8a24afbf038ee78a437b3
Kurtivan2223 <105442868+Kurtivan2223@users.noreply.github.com>
2023-11-15 18:12:23 +0800

    Added composer

--- include/database.php
+++ include/database.php
@@ -5,7 +5,12 @@ class Database
 
     public static function db_connection()
     {
-        self::$connection = new PDO("mysql:host=".get_config('db_auth_host').";dbname=".get_config('db_auth_dbname')."", get_config('db_auth_user'), get_config('db_auth_pass'));
+        self::$connection = new PDO(
+            "mysql:host=".get_config('db_auth_host').";dbname=".get_config('db_auth_dbname')."",
+            get_config('db_auth_user'),
+            get_config('db_auth_pass')
+        );
+        
         self::$connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         self::$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }
--- loader.php
+++ loader.php
@@ -2,6 +2,7 @@
     ob_start();
     session_start();
 
+    require_once "./vendor/autoload.php";
     require_once "./include/config.php";
     require_once "./include/functions.php";
     require_once "./include/Database.php";
